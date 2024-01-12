9c51892b91324c755639a84a493ebdf495b33ee5
Daniel Hernandez <danielandres65l@gmail.com>
2024-01-12 17:15:08 -0500

    Clustering strategy w/ MiniBatch

---
+++ k_means.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+from sklearn.cluster import MiniBatchKMeans
+
+if __name__ == '__main__':
+    dataset = pd.read_csv('./data/candy.csv')
+    print(dataset.head(10))
+
+    # Since this is unsupervised learning, test and train data split is not required.
+    X = dataset.drop('competitorname', axis=1)
+
+    kmeans = MiniBatchKMeans(n_clusters=4, batch_size=8).fit(X)
+    print("Total Centroids: ", len(kmeans.cluster_centers_))
+
+    print('='*32)
+    print(kmeans.predict(X))
+
+    dataset['group'] = kmeans.predict(X)
+
+    print(dataset)
